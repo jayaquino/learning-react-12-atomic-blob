48511d04b4fb9f5c9628a19eb3f90ad482ede24c
Nelson Aquino Jr. <naquinojr@outlook.com>
2025-02-17 15:30:00 -0800

    feature(context): add error if provider is used outside context

--- src/PostContext.js
+++ src/PostContext.js
@@ -51,6 +51,8 @@ function PostProvider({ children }) {
 
 function usePosts() {
   const context = useContext(PostContext);
+  if (context === undefined)
+    throw new Error("usePosts must be used within a PostProvider");
   return context;
 }
 
